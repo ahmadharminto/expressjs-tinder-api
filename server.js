8dcb73ec8a8a3804bf5035ab947659d50af5ee37
Ahmad Harminto <harminto_ahmad@live.com>
2021-01-03 13:13:43 +0700

    fix env

--- server.js
+++ server.js
@@ -1,14 +1,13 @@
-import dotenv from 'dotenv'
+import _ from './config.js';
 import express from 'express'
 import mongoose from 'mongoose'
 import bodyParser from 'body-parser'
 import routes from './routes.js'
 import Cors from 'cors'
 
-const env = dotenv.config();
 const app = express();
 const port = process.env.PORT || 8001;
-const connection_url = `mongodb+srv://${env.parsed.DB_USER}:${env.parsed.DB_PASS}@${env.parsed.DB_HOST}?retryWrites=true&w=majority`;
+const connection_url = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}?retryWrites=true&w=majority`;
 
 mongoose.connect(connection_url, {
     useNewUrlParser: true,
